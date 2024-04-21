f3688ba29ffd2c372a8fa880a90b8b9d7a9cd4de
aarticode <aartisingh1117@gmail.com>
2024-04-21 17:07:52 +0530

    Added day1 files

---
+++ day1.py
@@ -0,0 +1,28 @@
+#Creating a Comment
+#Comments starts with a #, and Python will ignore them:
+
+# This is a comment.
+print("Hello, World!")
+print("Hello, World!")
+
+
+#Multiline Comments
+#Python does not really have a syntax for multiline comments.
+#To add a multiline comment you could insert a # for each line:
+
+#Example
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#Or, not quite as intended, you can use a multiline string.
+#Since Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+
+#Example
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
