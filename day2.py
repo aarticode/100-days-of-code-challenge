7707462130e580fb0b01a77c8e47e17f47778554
aarticode <aartisingh1117@gmail.com>
2024-04-22 14:57:07 +0530

    Added day2.py with some changes

---
+++ day2.py
@@ -0,0 +1,11 @@
+#Day 2 - Our First Program
+#Today we will write our first ever python program from scratch. It will consist of a bunch of print statements.
+#print can be used to print something on the console in python
+
+
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+print(5+4)
+
