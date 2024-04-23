ff833fae61b21be814e3d53a593ecd143ac02208
aarticode <aartisingh1117@gmail.com>
2024-04-23 12:54:18 +0530

    Added day3 files

---
+++ day3modulesandpip.py
@@ -0,0 +1,12 @@
+#The pip command
+#It can be used as a package manager pip to install a python module. Lets install a module called pandas using the following command
+#pip install pandas
+
+
+#Using a module in Python (Usage)
+#We use the import syntax to import a module in Python. Here is an example code:
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
