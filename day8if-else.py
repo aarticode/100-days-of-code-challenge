f183a8fa54cd3785624ea3a95f4c8e4c309dae9f
aarticode <aartisingh1117@gmail.com>
2024-05-09 19:33:00 +0530

    If Else in One Line

--- day8if-else.py
+++ day8if-else.py
@@ -63,4 +63,26 @@ print("Your grade is:", grade)
 
 
 
+#If ... Else in One Line
+#There is also a shorthand syntax for the if-else statement that can be used when
+# the condition being tested is simple and the code blocks to be executed are short. Here's an example:
+a = 330000
+b = 3303
+print("A") if a > b else print("=") if a == b else print("B")
+c = 9 if a>b else 0
+print(c)
+
+
+#Example
+a=2
+b=330
+print ("A") if a>b else print ("B")
+
+
+#Example
+#One line if else statement, with 3 conditions:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b \
+    else print("B")
 
