65ae50c1149e90ba463abf3cb1e43ecd39a78424
aarticode <aartisingh1117@gmail.com>
2024-05-11 22:20:50 +0530

    else in Loop

--- else-if.py
+++ else-if.py
@@ -88,3 +88,42 @@ total_amount = purchase_amount * (1 - discount)
 print("Your total amount after discount is:", total_amount)
 
 
+
+#Python - else in Loop
+#As you have learned before, the else clause is used along with the if statement.
+#Python allows the else keyword to be used with the for and while loops too.
+# The else block appears after the body of the loop.
+# The statements in the else block will be executed after all iterations are completed.
+#Example1
+i = 0
+while i<7:
+  print(i)
+  i = i + 1
+else:
+  print("Sorry no i")
+
+#Example2
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
+
+#Example3
+for x in range(8):
+    print("Item  no {} found!" .format(x+1))
+
+else:
+    print("Item  found.")
+
+
+#Example4
+items = [1,2,3,4,5]
+for item in  items:
+    if item == 6:
+        print("Item found!")
+        break
+else:
+    print("Item  not  found.")
+
+
