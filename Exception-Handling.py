523d6ac4d2221276e19ced5d7ef4181b26ca9577
aarticode <aartisingh1117@gmail.com>
2024-05-12 13:47:56 +0530

    Finally Clause

--- Exception-Handling.py
+++ Exception-Handling.py
@@ -95,4 +95,35 @@ for item in my_list:
     except ZeroDivisionError:
         print("Cannot divide by zero in loop.")
 
+#Finally Clause
+#The finally code block is also a part of exception handling.
+# When we handle exception using the try and except block, we can include a finally block at the end.
+# The finally block is always executed, so it is generally used for doing the concluding tasks like closing file resources or closing database connection or may be ending the program execution with a delightful message.
+
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
+
+
+def func1():
+  try:
+    l = [1, 5, 6, 7]
+    i = int(input("Enter the index: "))
+    print(l[i])
+    return 1
+  except:
+    print("Some error occurred")
+    return 0
+
+  finally:
+    print("I am always executed")
+
+x = func1()
+print(x)
+
 
