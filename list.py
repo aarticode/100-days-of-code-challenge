6d0400ef0c3f5b98dafe6015bf936627383b18b2
aarticode <your_email@example.com>
2024-05-26 15:30:50 +0530

    some changes

--- list.py
+++ list.py
@@ -101,10 +101,12 @@ concatenated_list = list1 + list2
 print(concatenated_list)
 
 #Using the 'extend()' method:
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
+list1 = [1, 2, 3,4]
+list2 = [ 5, 6,7,8]
 list1.extend(list2)
 print(list1)
+
+
 
 
 
